51347a2416b5c0ee246e374f6d8d592bd7164906
Diego <novaessdiego@gmail.com>
2024-04-04 00:47:20 -0300

    feat aula 2 quase pronta

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,20 +1,32 @@
-import { Image, StatusBar, View } from 'react-native'
+import { Alert, Image, StatusBar, View } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { Link } from 'expo-router'
 
 import { Input } from '@/components/input'
 import { colors } from '@/styles/colors'
 import { Button } from '@/components/button'
+import { useState } from 'react'
 
 export default function Home() {
+  const [code, setCode] = useState("")
+
+  function handleAccessCredential() {
+    if (!code.trim()) {
+      return Alert.alert("Inválido", "Informe o código do ingresso")
+    }
+  }
+
   return (
     <View className='flex-1 bg-green-500 items-center justify-center p-8'>
+      
       <StatusBar barStyle='light-content' />
+      
       <Image
         source={require('@/assets/logo.png')}
         className='h-16'
         resizeMode='contain'
       />
+
       <View className='w-full mt-12 gap-3'>
         <Input>
           <MaterialCommunityIcons
@@ -22,9 +34,14 @@ export default function Home() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder='Código do ingresso'/>
+          <Input.Field
+            placeholder='Código do ingresso'
+            onChangeText={setCode}
+          />
         </Input>
-        <Button title='Acessar Credencial' />
+
+        <Button title='Acessar Credencial' onPress={handleAccessCredential}/>
+        
         <Link href='/register' className='text-gray-100 text-base font-bold text-center mt-8'> Ainda não possui ingresso?</Link>
       </View>
     </View>
--- src/app/register.tsx
+++ src/app/register.tsx
@@ -1,20 +1,34 @@
-import { Image, StatusBar, View } from 'react-native'
+import { Alert, Image, StatusBar, View } from 'react-native'
 import { FontAwesome6, MaterialIcons } from '@expo/vector-icons'
-import { Link } from 'expo-router'
+import { Link, router } from 'expo-router'
 
 import { Input } from '@/components/input'
 import { colors } from '@/styles/colors'
 import { Button } from '@/components/button'
+import { useState } from 'react'
 
 export default function Register() {
+  const [ name, setName ] = useState("")
+  const [ email, setEmail ] = useState("")
+
+  function handleRegister() {
+    if (!name.trim() || !email.trim()) {
+      return Alert.alert('Inválido', 'Preencha todos os campos')
+    }
+    router.push("/ticket")
+  }
+
   return (
     <View className='flex-1 bg-green-500 items-center justify-center p-8'>
+
       <StatusBar barStyle='light-content' />
+      
       <Image
         source={require('@/assets/logo.png')}
         className='h-16'
         resizeMode='contain'
       />
+      
       <View className='w-full mt-12 gap-3'>
         <Input>
           <FontAwesome6
@@ -22,7 +36,8 @@ export default function Register() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder='Nome completo'/>
+      
+          <Input.Field placeholder='Nome completo' onChangeText={setName}/>
         </Input>
 
         <Input>
@@ -31,10 +46,16 @@ export default function Register() {
             color={colors.green[200]}
             size={20}
           />
-          <Input.Field placeholder='E-mail' keyboardType='email-address'/>
+      
+          <Input.Field
+            placeholder='E-mail'
+            keyboardType='email-address'
+            onChangeText={setEmail}
+          />
         </Input>
 
-        <Button title='Realizar Inscrição' />
+        <Button title='Realizar Inscrição' onPress={handleRegister}/>
+
         <Link href='/' className='text-gray-100 text-base font-bold text-center mt-8'>
           Já possui ingresso?
         </Link>
